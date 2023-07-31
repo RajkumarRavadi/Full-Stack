fd599b8b8eb018ad5495bbc477c63bc808a5218c
RajkumarRavadi <rajk142567@gmail.com>
2023-07-31 23:16:59 +0530

    css selectors added

---
+++ Problem_Solving/area_of_rectangel.py
@@ -0,0 +1,8 @@
+#To find the area of a rectangle
+#we need to know about
+# height and width of rectangele
+
+length=int(input("Enter height: "))
+width=int(input("Enter width: "))
+area=length*width
+print("The area of rectangle is",area)
